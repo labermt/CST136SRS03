07198d85641b49241c63f073e221248af80ed4c7
Fennekin <colin.rexing@gmail.com>
2018-05-09 11:35:41 -0700

    adapting dimentions to work with currency

--- StoneMoney/convert.h
+++ StoneMoney/convert.h
@@ -14,5 +14,17 @@ template
 
 class Currency
 {
-	
+private:
+	double val;
+
+public:
+	explicit Currency(double initVal = 0): val(initVal) {}
+	double value() const { return val; }
+	double& value() { return val; }
+
+	template<typename T>
+	double convert(T value)
+	{
+		return ((value *1.0) * val) / 100;
+	}
 };
--- StoneMoney/main.cpp
+++ StoneMoney/main.cpp
@@ -1,16 +1,18 @@
 ﻿#include "stdafx.h"
 #include "convert.h"
 
-using US = Currency<1,0,0,0,0,0,0>;
-using AU = Currency<0,1,0,0,0,0,0>;
-using NZ = Currency<0,0,1,0,0,0,0>;
-using FJ = Currency<0,0,0,1,0,0,0>;
-using TO = Currency<0,0,0,0,1,0,0>;
-using PG = Currency<0,0,0,0,0,1,0>;
-using SB = Currency<0,0,0,0,0,0,1>;
+using US = Currency<100,0,0,0,0,0,0>;
+using AU = Currency<0,76,0,0,0,0,0>;
+using NZ = Currency<0,0,71,0,0,0,0>;
+using FJ = Currency<0,0,0,49,0,0,0>;
+using TO = Currency<0,0,0,0,45,0,0>;
+using PG = Currency<0,0,0,0,0,31,0>;
+using SB = Currency<0,0,0,0,0,0,13>;
 
 int main()
 {
+	US us;
+	auto result = us.convert(5);
     return 0;
 }
 

a4bc89022a8862b3afe49600ffbf670b0d5bca49
Fennekin <colin.rexing@gmail.com>
2018-05-15 08:07:18 -0700

    not sure how to call different constructors

--- StoneMoney/convert.h
+++ StoneMoney/convert.h
@@ -1,28 +1,31 @@
 #pragma once
 #include "stdafx.h"
-#include "countries.h"
 
 
+enum class Country { US, AU, NZ, FJ, TO, PG, SB };
 
-
-template<Countries name, int value>
+template<class Country, int Money>
 class Currency
 {
+private:
 	double val_{};
 
-	template<class US>
-	Currency()
+public:
+	template<Country US>
+	Currency() : val_{ Money * 1.0 }
 	{
-		val_{ 1.0 };
+	}
+
+	template<Country AU>
+	Currency():val_{Money * .76}
+	{
 	}
 
 	template<>
 	struct compiler_assert<false>;
-
-public:
+};
 	/*template<typename T, Currency<>
 	constexpr double convert(T value, currency to)
 	{
 		return((value * 1.0 ) / val_) * to.val_;
 	}*/
-};
--- StoneMoney/countries.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-enum class Countries{US,AU,NZ,FJ,TO,PG,SB};

e7b6a62854e716e6f83bd71ed70c780286a152a9
Fennekin <colin.rexing@gmail.com>
2018-05-17 17:16:15 -0700

    on my way

--- StoneMoney/convert.h
+++ StoneMoney/convert.h
@@ -4,28 +4,104 @@
 
 enum class Country { US, AU, NZ, FJ, TO, PG, SB };
 
-template<class Country, int Money>
+template<Country country>
+class Exchange;
+
+
+template<>
+class Exchange<Country::US>
+{
+public:
+	static constexpr double rate = 1.0;
+
+}; 
+
+template<>
+class Exchange<Country::AU>
+{
+public:
+	static constexpr double rate = .76;
+
+};
+
+template<>
+class Exchange<Country::NZ>
+{
+public:
+	static constexpr double rate = .71;
+
+};
+
+template<>
+class Exchange<Country::FJ>
+{
+public:
+	static constexpr double rate = .49;
+
+};
+
+template<>
+class Exchange<Country::TO>
+{
+public:
+	static constexpr double rate = .45;
+
+};
+
+template<>
+class Exchange<Country::PG>
+{
+public:
+	static constexpr double rate = .31;
+
+};
+
+template<>
+class Exchange<Country::SB>
+{
+public:
+	static constexpr double rate = .13;
+
+};
+
+
+template<Country nation>
 class Currency
 {
+	using Money = std::pair<int, int>;
+
+
 private:
-	double val_{};
+	double usd_val_{};
+
+	
 
 public:
-	template<Country US>
-	Currency() : val_{ Money * 1.0 }
+
+	constexpr Currency(Money money) : usd_val_{ money.first + (money.second / 100.0) }
 	{
 	}
 
-	template<Country AU>
+	constexpr explicit Currency(Currency<Country::NZ> from_currency): usd_val_{from_currency.get_money()}
+	{
+	}
+
+	constexpr double get_money() const
+	{
+		return usd_val_;
+	}
+	
+	/*template<Country AU>
 	Currency():val_{Money * .76}
 	{
-	}
-
-	template<>
-	struct compiler_assert<false>;
-};
-	/*template<typename T, Currency<>
-	constexpr double convert(T value, currency to)
-	{
-		return((value * 1.0 ) / val_) * to.val_;
 	}*/
+
+	//template<>
+	//struct compiler_assert<false>;
+
+	//template<typename T, Currency<>
+	//	constexpr double convert(currency to)
+	//	{
+	//		return((value * 1.0) / val_) * to.val_;
+	//	}
+};

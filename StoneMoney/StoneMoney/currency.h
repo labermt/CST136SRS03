2bb1d729398952cd24c748af73d71dc17df395da
Thea Van Kessel <thea.vankessel@gmail.com>
2018-05-18 13:22:47 -0700

    Working Solution

--- StoneMoney/StoneMoney/currency.h
+++ StoneMoney/StoneMoney/currency.h
@@ -4,20 +4,122 @@
 #include "country.h"
 
 //Make compiler get mad at you if you have a country that you don't have!
+//Rounding errors
 
-template <Country C, int Money> class Currency;
+//Conversions from US to other countries
+template <Country C, int Money> class Converted;
 
-//Base conversion of US dollars
 template <int Money>
-class Currency<Country::US, Money>
+class Converted<Country::AU, Money>
 {
+private:
+    static constexpr int conversion{76};
+    
 public:
-    static constexpr int value{Money};
+    static constexpr int usToAu()
+    {
+        return (Money * conversion) / 100;
+    }
+    static constexpr int value{usToAu()};
 };
 
 
+
 template <int Money>
-class Currency<Country::AU, Money>
+class Converted<Country::NZ, Money>
+{
+private:
+    static constexpr int conversion{71};
+    
+public:
+    static constexpr int usToNz()
+    {
+        return (Money * conversion) / 100;
+    }
+    static constexpr int value{usToNz()};
+};
+
+
+
+template <int Money>
+class Converted<Country::FJ, Money>
+{
+private:
+    static constexpr int conversion{49};
+    
+public:
+    static constexpr int usToFj()
+    {
+        return (Money * conversion) / 100;
+    }
+    static constexpr int value{usToFj()};
+};
+
+
+
+template <int Money>
+class Converted<Country::TO, Money>
+{
+private:
+    static constexpr int conversion{45};
+    
+public:
+    static constexpr int usToTo()
+    {
+        return (Money * conversion) / 100;
+    }
+    static constexpr int value{usToTo()};
+};
+
+
+
+template <int Money>
+class Converted<Country::PG, Money>
+{
+private:
+    static constexpr int conversion{31};
+    
+public:
+    static constexpr int usToPg()
+    {
+        return (Money * conversion) / 100;
+    }
+    static constexpr int value{usToPg()};
+};
+
+
+
+template <int Money>
+class Converted<Country::SB, Money>
+{
+private:
+    static constexpr int conversion{13};
+    
+public:
+    static constexpr int usToSb()
+    {
+        return (Money * conversion) / 100;
+    }
+    static constexpr int value{usToSb()};
+};
+
+
+
+//Conversions to US currency
+template <Country C, Country Co, int Money> class Currency;
+
+//Base conversion of US dollars
+template <Country Co, int Money>
+class Currency<Country::US, Co, Money>
+{
+public:
+    static constexpr int value{Money};
+    static constexpr int converted{Converted<Co, value>::value};
+};
+
+
+template <Country Co, int Money>
+class Currency<Country::AU, Co, Money>
 {
 private:
     static constexpr int conversion{76};
@@ -28,12 +130,13 @@ public:
         return (Money * 100) / conversion ;
     }
     static constexpr int value{usConversion()};
-
+    static constexpr int converted{Converted<Co, value>::value};
 };
 
 
-template <int Money>
-class Currency<Country::NZ, Money>
+
+template <Country Co, int Money>
+class Currency<Country::NZ, Co, Money>
 {
 private:
     static constexpr int conversion{71};
@@ -44,11 +147,13 @@ public:
         return (Money * 100) / conversion;
     }
     static constexpr int value{usConversion()};
+    static constexpr int converted{Converted<Co, value>::value};
 };
 
 
-template <int Money>
-class Currency<Country::FJ, Money>
+
+template <Country Co, int Money>
+class Currency<Country::FJ, Co, Money>
 {
 private:
     static constexpr int conversion{49};
@@ -59,11 +164,13 @@ public:
         return (Money * 100) / conversion;
     }
     static constexpr int value{usConversion()};
+    static constexpr int converted{Converted<Co, value>::value};
 };
 
 
-template <int Money>
-class Currency<Country::TO, Money>
+
+template <Country Co, int Money>
+class Currency<Country::TO, Co, Money>
 {
 private:
     static constexpr int conversion{45};
@@ -74,11 +181,12 @@ public:
         return (Money * 100) / conversion;
     }
     static constexpr int value{usConversion()};
+    static constexpr int converted{Converted<Co, value>::value};
 };
 
 
-template <int Money>
-class Currency<Country::PG, Money>
+template <Country Co, int Money>
+class Currency<Country::PG, Co, Money>
 {
 private:
     static constexpr int conversion{31};
@@ -89,11 +197,13 @@ public:
         return (Money * 100) / conversion;
     }
     static constexpr int value{usConversion()};
+    static constexpr int converted{Converted<Co, value>::value};
 };
 
 
-template <int Money>
-class Currency<Country::SB, Money>
+
+template <Country Co, int Money>
+class Currency<Country::SB, Co, Money>
 {
 private:
     static constexpr int conversion{13};
@@ -104,6 +214,7 @@ public:
         return (Money * 100) / conversion;
     }
     static constexpr int value{usConversion()};
+    static constexpr int converted{Converted<Co, value>::value};
 };
 
 

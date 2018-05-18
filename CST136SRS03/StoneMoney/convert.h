2de519044a1214a6cb9e92fca52dced41eb8f00f
ajstacher <ajstacher@gmail.com>
2018-05-18 13:37:56 -0700

    Final commit

--- CST136SRS03/StoneMoney/convert.h
+++ CST136SRS03/StoneMoney/convert.h
@@ -6,22 +6,11 @@ constexpr auto convert (const Sum s, const CurrencyValue base, const CurrencyVal
 {	
 	assert(static_cast<double>(base) >= 0 && static_cast<double>(target) >= 0);
 
-	auto sum = 0.0;
-	sum = s;
-	auto base_ = 0.0;
-	auto target_ = 0.0;
-
-	//if passed in values are enums Currency
-	if(static_cast<double>(base) > 1.0 && static_cast<double>(target) > 1.0)
-	{
-		base_ = static_cast<double>(base) / 100;
-		target_ = static_cast<double>(target) / 100;
-	}else
-	{
-		base_ = static_cast<double>(base);
-		target_ = static_cast<double>(target);
-	}
+	auto sum = s;
 	
-	return sum = (sum * base_) / target_;
+	const auto baseC = static_cast<double>(base) / 100;
+	const auto targetC = static_cast<double>(target) / 100;
+	
+	return sum = (sum * baseC) / targetC;
 }
 
---
+++ CST136SRS03/StoneMoney/currency.h
@@ -0,0 +1,4 @@
+#pragma once
+enum class Currency { us = 100, au = 76, nz = 71, fj = 49, to = 45, pg = 31, sb = 13 };
+
+

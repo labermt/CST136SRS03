f7f764e09abe20abf8f1b900e2b17f0d6154712d
generalwax2018 <generalwax2018@users.noreply.github.com>
2018-05-19 07:57:53 -0700

    removed case statement
    over complicating

--- CST136SRS03/StonyMoney/currencyConvert.h
+++ CST136SRS03/StonyMoney/currencyConvert.h
@@ -78,50 +78,6 @@ struct cCodes <countryCode::SB>
 	 
 };
 
-template <countryCode> 
-struct mycountry
-{
-public: 
-
-		constexpr countryCode country;
-
-
-	explicit mycountry(countryCode country)
-		: country(country)
-	{
-	}
-
-	constexpr auto find (mycountry)
-	{
-		double cCurrency = 0.0;
-
-		switch (country)
-		{
-		case countryCode::US:
-			cCurrency = 1.0;
-			break;
-		case countryCode::AU:
-			cCurrency = 0.76;
-			break; 
-		case countryCode::NZ:
-			cCurrency = 0.71;
-			break; 
-		case countryCode::FJ:
-			cCurrency = 0.49;
-			break;
-		case countryCode::TO:
-			cCurrency = 0.45;
-			break;
-		case countryCode::SB:
-			cCurrency = 0.13;
-			break;
-		default:
-			break; 
-		}
-
-		return cCurrency;
-
-
 
 template  <countryCode, int money, mycountry>
 	struct currencyExchange
@@ -132,4 +88,3 @@ template  <countryCode, int money, mycountry>
 
 	};
 
-};

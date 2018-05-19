af91a0e371d5d55633f6bc2f6e8dcf3894deb06c
Fennekin <colin.rexing@gmail.com>
2018-05-18 23:01:38 -0700

    pulled mitchs changes

--- StoneMoney/main.cpp
+++ StoneMoney/main.cpp
@@ -3,18 +3,16 @@
 
 int main()
 {
-	Currency<Country::US,Money<5,0>> test;
-	auto us_money =  test.val_;
-	auto us_nation = test.nation_;
-	Currency<Country::NZ, Money<10,50>> test_2;
-	auto nz_money = test_2.val_;
-	auto nz_nation = test_2.nation_;
-	Currency<Country::NZ, Currency<Country::US, Money<10, 50>>> test_3;
-	auto to_money = test_3.val_;
-	auto to_nation = test_3.nation_;
-	Currency<Country::NZ, Currency<Country::SB, Currency<Country::NZ, Money<10, 50>>>> test_4;
-	auto sb_money = test_4.val_;
-	auto sb_nation = test_4.nation_;
+
+	Currency<Country::NZ, Money<10,50>> test_1;
+	auto nz_money = test_1.val_;
+	auto nz_nation = test_1.nation_;
+	Currency<Country::NZ, Currency<Country::US, Money<10, 50>>> test_2;
+	auto to_money = test_2.val_;
+	auto to_nation = test_2.nation_;
+	Currency<Country::NZ, Currency<Country::SB, Currency<Country::NZ, Money<10, 50>>>> test_3;
+	auto sb_money = test_3.val_;
+	auto sb_nation = test_3.nation_;
 	
     return 0;
 }

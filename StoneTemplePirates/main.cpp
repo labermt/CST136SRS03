e7acc6f6d5ea6811811f3d6b25e972d19d0d0d1d
wolf9970 <38161200+wolf9970@users.noreply.github.com>
2018-05-19 06:13:15 -0700

    Update main.cpp

--- StoneTemplePirates/main.cpp
+++ StoneTemplePirates/main.cpp
@@ -4,7 +4,7 @@ int main()
 {
   double test{convert(5.0, Rate[Country::AU], Rate[Country::NZ])};
 
-  double test2{convert(20.0, Rate[Country::TO], Rate[Country::PG])};
+  double test2{convert(20.0, Rate[Country::TO])};
 
   double test3{convert(10.0, Rate[Country::US], Rate[Country::SB],
                              Rate[Country::FJ], Rate[Country::US])};

ef1f7364f75984f207a45d864348876a5bb96110
generalwax2018 <generalwax2018@users.noreply.github.com>
2018-05-19 06:01:51 -0700

    more baby steps

--- CST136SRS03/StonyMoney/currencyConvert.h
+++ CST136SRS03/StonyMoney/currencyConvert.h
@@ -19,7 +19,7 @@ struct currency
 	static_assert(cents < -1,"Negative number. Invalid input");
 	static_assert(dollars == 0, "Number is zero. Invalid input.");
 	static_assert(cents == 0, "Number is zero. Invalid input."); 
-	constexpr auto currency_= money; 
+	constexpr auto currency_= dollars + cents; 
 	
 };
 
@@ -39,34 +39,36 @@ public:
 
 	constexpr auto find (mycountry)
 	{
+		double cCurrency = 0.0;
+
 		switch (country)
 		{
 		case countryCode::US:
-			return 1.0;
+			cCurrency = 1.0;
 			break;
 		case countryCode::AU:
-			return 0.76;
+			cCurrency = 0.76;
 			break; 
 		case countryCode::NZ:
-			return 0.71;
+			cCurrency = 0.71;
 			break; 
 		case countryCode::FJ:
-			return 0.49;
+			cCurrency = 0.49;
 			break;
 		case countryCode::TO:
-			return 0.45;
+			cCurrency = 0.45;
 			break;
 		case countryCode::SB:
-			return 0.13;
+			cCurrency = 0.13;
 			break;
 		default:
 			break; 
 		}
 
-		return;
+		return cCurrency;
 	}
 
-template  <countryCode, int dollars, int cents>
+template  <countryCode, int money>
 	struct currencyExchange
 	{
 		
--- REPORT.md
+++ REPORT.md
@@ -4,6 +4,7 @@ Stone Template Pirates
 
 Student Name: Michael Waxman
 
-Issues: Plenty and cannot all be listed here. 
+Issues: stuck point: 
+1. pass the country currency and do the conversion using a template. 
 
 Notes:
